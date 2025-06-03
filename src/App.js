ffd8f7b6c6e416015d5ca564b3fc9769598951f3
pankaj971989 <pankajsingh89@gmail.com>
2025-06-03 18:50:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,12 @@ import Home from "./Component/Desktop/Home";
 function App() {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 992);
 
-  
   useEffect(() => {
     const handleResize = () => setIsMobile(window.innerWidth < 992);
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  
   return (
     <RouterProvider
       router={createBrowserRouter([
